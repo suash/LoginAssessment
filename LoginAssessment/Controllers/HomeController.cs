18aee1feda75aad6d3671299b7b7ab368d9373ba
Suash Maharaj <suash.maharaj@gmail.com>
2018-11-03 17:01:31 +0200

    Initial commit

---
+++ LoginAssessment/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoginAssessment.Models;
+
+namespace LoginAssessment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult PasswordReset()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoginAssessment/wwwroot/js/site.js
@@ -0,0 +1,189 @@
+﻿function register(postData, handleLogin) {
+    $.ajax({
+        url: "./register",
+        contentType: "application/json",
+        type: 'POST',
+        data: JSON.stringify(postData),
+        dataType: "JSON",
+        success: function (res) {
+            let response = {
+                success: true,
+                response: res
+            };
+
+            return handleLogin(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            let response = {
+                success: false,
+                code: xhr.status,
+                response: xhr.statusText
+            };
+
+            return handleLogin(response);
+        }
+    });
+}
+
+function login_password(postData, handleLogin) {
+    $.ajax({
+        url: "./loginpass",
+        contentType: "application/json",
+        type: 'POST',
+        data: JSON.stringify(postData),
+        dataType: "JSON",
+        success: function (res) {
+            let response = {
+                success: true,
+                response: res
+            };
+
+            return handleLogin(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            let response = {
+                success: false,
+                code: xhr.status,
+                response: xhr.statusText
+            };
+
+            return handleLogin(response);
+        }
+    });
+}
+
+function login_passphrase(postData, handleLogin) {
+    $.ajax({
+        url: "./loginphrase",
+        contentType: "application/json",
+        type: 'POST',
+        data: JSON.stringify(postData),
+        dataType: "JSON",
+        success: function (res) {
+            let response = {
+                success: true,
+                response: res
+            };
+
+            return handleLogin(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            let response = {
+                success: false,
+                code: xhr.status,
+                response: xhr.statusText
+            };
+
+            return handleLogin(response);
+        }
+    });
+}
+
+function update_password(postData, handleLogin) {
+    $.ajax({
+        url: "./updatepass",
+        contentType: "application/json",
+        type: 'POST',
+        data: JSON.stringify(postData),
+        dataType: "JSON",
+        success: function (res) {
+            let response = {
+                success: true,
+                response: res
+            };
+
+            return handleLogin(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            let response = {
+                success: false,
+                code: xhr.status,
+                response: xhr.statusText
+            };
+
+            return handleLogin(response);
+        }
+    });
+}
+
+function update_passphrase(postData, handleLogin) {
+    $.ajax({
+        url: "./updatephrase",
+        contentType: "application/json",
+        type: 'POST',
+        data: JSON.stringify(postData),
+        dataType: "JSON",
+        success: function (res) {
+            let response = {
+                success: true,
+                response: res
+            };
+
+            return handleLogin(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            let response = {
+                success: false,
+                code: xhr.status,
+                response: xhr.statusText
+            };
+
+            return handleLogin(response);
+        }
+    });
+}
+
+
+function failed_login(postData, handleLogin) {
+    $.ajax({
+        url: "./failedlogin",
+        contentType: "application/json",
+        type: 'POST',
+        data: JSON.stringify(postData),
+        dataType: "JSON",
+        success: function (res) {
+            let response = {
+                success: true,
+                response: res
+            };
+
+            return handleLogin(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            let response = {
+                success: false,
+                code: xhr.status,
+                response: xhr.statusText
+            };
+
+            return handleLogin(response);
+        }
+    });
+}
+
+function success_login(postData, handleLogin) {
+    $.ajax({
+        url: "./successlogin",
+        contentType: "application/json",
+        type: 'POST',
+        data: JSON.stringify(postData),
+        dataType: "JSON",
+        success: function (res) {
+            let response = {
+                success: true,
+                response: res
+            };
+
+            return handleLogin(response);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            let response = {
+                success: false,
+                code: xhr.status,
+                response: xhr.statusText
+            };
+
+            return handleLogin(response);
+        }
+    });
+}
