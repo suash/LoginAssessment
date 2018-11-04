2142621db0c987fae596a7009f7d91b1dd2abd8c
Suash Maharaj <suash.maharaj@gmail.com>
2018-11-04 23:14:49 +0200

    Added steps 1 & 2 validation for the registration page.

--- LoginAssessment/wwwroot/js/site.js
+++ LoginAssessment/wwwroot/js/site.js
@@ -1,189 +1,75 @@
-﻿function register(postData, handleLogin) {
-    $.ajax({
-        url: "./register",
-        contentType: "application/json",
-        type: 'POST',
-        data: JSON.stringify(postData),
-        dataType: "JSON",
-        success: function (res) {
-            let response = {
-                success: true,
-                response: res
-            };
+﻿angular.module('assessment', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
-            return handleLogin(response);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            let response = {
-                success: false,
-                code: xhr.status,
-                response: xhr.statusText
-            };
+angular.module('assessment').controller('RegisterController', function ($scope, $window) {
+    $scope.step2Disabled = true;
+    $scope.step3Disabled = true;
+    $scope.step4Disabled = true;
 
-            return handleLogin(response);
+    $scope.canProceedToStep2 = function () {
+        return !$scope.step2Disabled;
+    };
+
+    $scope.canProceedToStep3 = function () {
+        return !$scope.step3Disabled;
+    };
+
+    $scope.validateStep1 = function () {
+        var form = $scope.registerForm;
+        var isvalid = form.age.$modelValue > 0 && form.gender.$modelValue >= 0 && form.email.$modelValue;
+        $scope.step2Disabled = !isvalid;
+    };
+
+    $scope.validateStep2 = function () {
+        var isvalid = $scope.isValidLength &&
+            $scope.hasSpecialCharacter &&
+            $scope.hasDigit &&
+            $scope.hasLowercaseLetter &&
+            $scope.hasUppercaseLetter &&
+            !$scope.passwordsNotMatching;
+
+        $scope.step3Disabled = !isvalid;
+    };
+
+    $scope.goToStep1 = function () {
+        $scope.active = 0;
+    };
+    
+    $scope.goToStep2 = function () {
+        $scope.active = 1;
+    };
+
+    $scope.goToStep3 = function () {
+        $scope.active = 2;
+    };
+
+    $scope.goToStep4 = function () {
+        $scope.active = 3;
+    };
+
+    $scope.password1Change = function () {
+        var form = $scope.registerForm;
+        if (form.password.$modelValue) {
+            var passwordValue = form.password.$modelValue;
+            $scope.isValidLength = passwordValue.length > 8;
+            $scope.hasSpecialCharacter = new RegExp('[`~!@#$%^&*()\\-\\_=+,<.>]').test(passwordValue);
+            $scope.hasDigit = new RegExp('[0-9]').test(passwordValue);
+            $scope.hasLowercaseLetter = new RegExp('[a-z]').test(passwordValue);
+            $scope.hasUppercaseLetter = new RegExp('[A-Z]').test(passwordValue);
+        } else {
+            $scope.isValidLength = false;
+            $scope.hasSpecialCharacter = false;
+            $scope.hasDigit = false;
+            $scope.hasLowercaseLetter = false;
+            $scope.hasUppercaseLetter = false;
         }
-    });
-}
 
-function login_password(postData, handleLogin) {
-    $.ajax({
-        url: "./loginpass",
-        contentType: "application/json",
-        type: 'POST',
-        data: JSON.stringify(postData),
-        dataType: "JSON",
-        success: function (res) {
-            let response = {
-                success: true,
-                response: res
-            };
+        $scope.validateStep2();
+    };
 
-            return handleLogin(response);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            let response = {
-                success: false,
-                code: xhr.status,
-                response: xhr.statusText
-            };
+    $scope.password2Change = function () {
+        var form = $scope.registerForm;
+        $scope.passwordsNotMatching = form.password.$modelValue !== form.password2.$modelValue;
 
-            return handleLogin(response);
-        }
-    });
-}
-
-function login_passphrase(postData, handleLogin) {
-    $.ajax({
-        url: "./loginphrase",
-        contentType: "application/json",
-        type: 'POST',
-        data: JSON.stringify(postData),
-        dataType: "JSON",
-        success: function (res) {
-            let response = {
-                success: true,
-                response: res
-            };
-
-            return handleLogin(response);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            let response = {
-                success: false,
-                code: xhr.status,
-                response: xhr.statusText
-            };
-
-            return handleLogin(response);
-        }
-    });
-}
-
-function update_password(postData, handleLogin) {
-    $.ajax({
-        url: "./updatepass",
-        contentType: "application/json",
-        type: 'POST',
-        data: JSON.stringify(postData),
-        dataType: "JSON",
-        success: function (res) {
-            let response = {
-                success: true,
-                response: res
-            };
-
-            return handleLogin(response);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            let response = {
-                success: false,
-                code: xhr.status,
-                response: xhr.statusText
-            };
-
-            return handleLogin(response);
-        }
-    });
-}
-
-function update_passphrase(postData, handleLogin) {
-    $.ajax({
-        url: "./updatephrase",
-        contentType: "application/json",
-        type: 'POST',
-        data: JSON.stringify(postData),
-        dataType: "JSON",
-        success: function (res) {
-            let response = {
-                success: true,
-                response: res
-            };
-
-            return handleLogin(response);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            let response = {
-                success: false,
-                code: xhr.status,
-                response: xhr.statusText
-            };
-
-            return handleLogin(response);
-        }
-    });
-}
-
-
-function failed_login(postData, handleLogin) {
-    $.ajax({
-        url: "./failedlogin",
-        contentType: "application/json",
-        type: 'POST',
-        data: JSON.stringify(postData),
-        dataType: "JSON",
-        success: function (res) {
-            let response = {
-                success: true,
-                response: res
-            };
-
-            return handleLogin(response);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            let response = {
-                success: false,
-                code: xhr.status,
-                response: xhr.statusText
-            };
-
-            return handleLogin(response);
-        }
-    });
-}
-
-function success_login(postData, handleLogin) {
-    $.ajax({
-        url: "./successlogin",
-        contentType: "application/json",
-        type: 'POST',
-        data: JSON.stringify(postData),
-        dataType: "JSON",
-        success: function (res) {
-            let response = {
-                success: true,
-                response: res
-            };
-
-            return handleLogin(response);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            let response = {
-                success: false,
-                code: xhr.status,
-                response: xhr.statusText
-            };
-
-            return handleLogin(response);
-        }
-    });
-}
+        $scope.validateStep2();
+    };
+});
